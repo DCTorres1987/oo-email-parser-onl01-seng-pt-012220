6b8461296d0f5a28bbf0c26deb853990d84785cb
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-02-14 00:51:08 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,7 +7,11 @@ require 'pry'
 
 class EmailAddressParser
   
-  attr_accessor :emails 
+  attr_accessor :email_address
+  
+  def initialize(email_address)
+    @email_address = email_address
+  end
   
 
 end
